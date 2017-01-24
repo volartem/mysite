5f1d8d12beeac05a4faedebcd206ed4f8e590a9f
volartem <volartem7@gmail.com>
2017-01-24 14:55:45 +0200

    Notes with text and comments

--- mysite/settings.py
+++ mysite/settings.py
@@ -164,11 +164,22 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
     'social_core.pipeline.user.create_user',
-    'visitor.pipeline.save_user_data',  # <--- set the path to the function
     'social_core.pipeline.social_auth.associate_user',
+    'visitor.pipeline.save_user_data',  # <--- set the path to the function
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
+
+SOCIAL_AUTH_SANITIZE_REDIRECTS = True
+SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/'
+
+SOCIAL_AUTH_GITHUB_SCOPE = ['user:email']
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
+  'locale': 'ru_RU',
+  'fields': 'id, name, email'
+}
+
 
 
 try:
--- note/views.py
+++ note/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Note
 # Create your views here.
 
 def note_detail(request, pk):
-    return render(request, 'note/note.html')
+    note = Note.objects.get(id=pk)
+    return render(request, 'note/note.html', {'note': note})
