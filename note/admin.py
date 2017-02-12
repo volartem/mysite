07db71e47383b039b5f4eab2fa3f5c0945ac0309
volartem <volartem7@gmail.com>
2017-02-12 21:44:33 +0200

    small fix

--- note/admin.py
+++ note/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Note, Comment
+from .models import Note, Comment, Something
 
 
 admin.site.register(Note)
 admin.site.register(Comment)
+admin.site.register(Something)
