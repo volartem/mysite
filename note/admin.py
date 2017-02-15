c022d71fedace141db6efac03082a5fa84a039e7
volartem <volartem7@gmail.com>
2017-02-15 22:12:21 +0200

    added title and format time

--- note/admin.py
+++ note/admin.py
@@ -3,7 +3,13 @@ from .models import Note, Comment, Something
 
 
 class SomethingAdmin(admin.ModelAdmin):
-    readonly_fields = ('date', )
+    readonly_fields = ('format_date', )
+    list_display = ('path', 'format_date', 'method', 'status_code', 'ip')
+
+    def format_date(self, obj):
+        return obj.date.strftime('%H:%M:%S %d-%m-%Y')
+
+    format_date.short_description = 'Visited time'
 
 admin.site.register(Note)
 admin.site.register(Comment)
