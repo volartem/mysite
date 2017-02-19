72518cbc7d071e7ff5a13e46dc99538015e82dbc
volartem <volartem7@gmail.com>
2017-02-19 18:29:26 +0200

    disabled in urls create comment

--- note/urls.py
+++ note/urls.py
@@ -4,6 +4,6 @@ from note import views
 
 urlpatterns = [
     url(r'^(?P<pk>\d+)/$', views.note_detail, name='note'),
-    url(r'^add/comment/(?P<pk>\d+)$', views.add_comment, name='add_comment'),
+    # url(r'^add/comment/(?P<pk>\d+)$', views.add_comment, name='add_comment'),
     url(r'(?P<pk>\d+)/comments/$', views.CommentList.as_view(), name='note_comments'),
 ]
