332d4db0f086cfd49d2cff6923b3c4e01c96e051
volartem <volartem7@gmail.com>
2017-03-23 18:44:32 +0200

    new logger

--- note/middleware.py
+++ note/middleware.py
@@ -15,8 +15,24 @@ class SimpleMiddleware(object):
         return response
 
     def process_response(self, request, response):
-        try:
-            logger.warning("%s ;; %s" % (request.path, request.META))
-        except:
-            pass
+        logger.warning("%s ;;; %s" % (request.path, get_info(request, response)))
         return response
+
+
+def get_info(request, response):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    try:
+        refer = request.META.get('HTTP_REFERER')[:255]
+    except TypeError:
+        refer = 'not_refer'
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[-1].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    obj = {
+        'status_code': response.status_code,
+        'method': request.method,
+        'ip': ip,
+        'refer': refer,
+    }
+    return obj
